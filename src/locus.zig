d0661acc766a909f82b0c9f86307b696fbe31c0e
Zack Kollar <rallokkcaz@gmail.com>
2023-11-30 02:20:00 -0500

    :hammer: Parse zig source files

--- src/locus.zig
+++ src/locus.zig
@@ -17,24 +17,13 @@ const Context = struct {
     pub fn deinit(_: *Context) void {}
 };
 
-var context: Context = undefined;
-
-pub fn init(allocator: std.mem.Allocator, locale: []const u8) !Context {
-    context = try Context.init(allocator, locale);
-
-    return context;
-}
-
-pub fn translate(msgid: []const u8) []const u8 {
-    return msgid;
-}
-
 const TemplateGenerator = struct {
     const Self = @This();
 
     allocator: std.mem.Allocator,
     locale: []const u8,
     file_arena: std.heap.ArenaAllocator,
+    has_errors: bool = false,
 
     pub fn init(allocator: std.mem.Allocator, locale: []const u8) TemplateGenerator {
         return .{
@@ -49,22 +38,35 @@ const TemplateGenerator = struct {
     }
 
     pub fn generate(self: *Self, root: []const u8) !void {
-        std.log.info("Processing directory: {s}", .{root});
+        std.log.debug("Processing directory: {s}", .{root});
 
         // Open the root directory.
         var root_dir = try std.fs.cwd().openDir(root, .{ .iterate = true });
         defer root_dir.close();
 
-        // Walk the directory tree.
+        // Walk the directory tree and process each file.
         var dir_iterator = root_dir.iterate();
         while (try dir_iterator.next()) |entry| {
             if (entry.kind == .file) {
                 const path = try self.pathFromRoot(root, entry.name);
-                try self.processFile(path);
+                defer self.allocator.free(path);
+
+                std.log.info("Processing file: {s}", .{path});
+                self.processFile(path) catch {
+                    std.log.err("Error processing file: {s}", .{path});
+                    self.has_errors = true;
+                };
             } else if (entry.kind == .directory) {
                 const path = try self.pathFromRoot(root, entry.name);
+                defer self.allocator.free(path);
+
                 try self.generate(path);
             }
+        }
+
+        // Fail if there were any errors.
+        if (self.has_errors) {
+            return error.ParseError;
         }
     }
 
@@ -73,34 +75,58 @@ const TemplateGenerator = struct {
     fn pathFromRoot(self: *Self, root: []const u8, file_path: []const u8) ![]const u8 {
         // Relative path to the file.
         // Format the relative path to the file.
-        const path = try std.fmt.allocPrint(self.allocator, "{s}/{s}", .{ root, file_path });
-        std.log.info("Processing file: {s}\n", .{path});
-
-        return path;
+        return try std.fmt.allocPrint(self.allocator, "{s}/{s}", .{ root, file_path });
     }
 
     fn processFile(self: *Self, path: []const u8) !void {
-        defer self.allocator.free(path);
-
         // Check if the file is a zig file.
         if (!isZigFile(path)) {
-            std.log.debug("Skipping non-zig file: {s}\n", .{path});
+            std.log.debug("Skipping non-zig file: {s}", .{path});
             return;
         }
 
-        // Get the file contents.
-        const file_contents = try self.getFileContents(path);
+        // Get the file contents, don't need to defer because it looks like AST parse will
+        // deallocate the source.
+        const source = try self.readSource(path);
 
-        // Print the file for now
-        std.debug.print("{s}\n", .{file_contents});
+        // Get the AST.
+        var ast = try std.zig.Ast.parse(self.allocator, source, .zig);
+        defer ast.deinit(self.allocator);
+
+        // Print the errors if there are any.
+        try printAstErrors(path, source, &ast);
+        // Fail if there are any errors.
+        if (ast.errors.len != 0) {
+            return error.ParseError;
+        }
     }
 
-    fn isZigFile(file_path: []const u8) bool {
+    fn printAstErrors(path: []const u8, source: [:0]const u8, ast: *std.zig.Ast) !void {
+        // Make sure the ast doesn't have errors.
+        // This is copied from the zig codebase:
+        // https://github.com/ziglang/zig/blob/master/lib/std/zig/parser_test.zig#L6175C5-L6191C6
+        for (ast.errors) |parse_error| {
+            const loc = ast.tokenLocation(0, parse_error.token);
+            std.debug.print("{s}:{d}:{d}: error: ", .{ path, loc.line + 1, loc.column + 1 });
+            try ast.renderError(parse_error, std.io.getStdErr().writer());
+            std.debug.print("\n{s}\n", .{source[loc.line_start..loc.line_end]});
+            {
+                var i: usize = 0;
+                while (i < loc.column) : (i += 1) {
+                    std.debug.print(" ", .{});
+                }
+                std.debug.print("^", .{});
+            }
+            std.debug.print("\n", .{});
+        }
+    }
+
+    inline fn isZigFile(file_path: []const u8) bool {
         // Check the file extension is ".zig".
         return std.mem.endsWith(u8, file_path, ".zig");
     }
 
-    fn getFileContents(self: *Self, path: []const u8) ![]const u8 {
+    fn readSource(self: *Self, path: []const u8) ![:0]const u8 {
         // ======================================================================================
         // Clear the file arena.
         // ======================================================================================
@@ -123,23 +149,38 @@ const TemplateGenerator = struct {
         // Get the size of the file.
         const file_size = (try file.stat()).size;
 
-        // Allocate a buffer for the file contents.
-        const file_contents = try allocator.alloc(u8, file_size);
-
         // Read the file contents into the buffer.
-        const bytes_read = try file.readAll(file_contents);
-
-        // Assert we read the entire file.
-        if (bytes_read != file_size) {
-            std.log.err("Failed to read entire file: {s}\n", .{path});
-            return error.FileReadFailed;
-        }
+        const file_contents = try file.readToEndAllocOptions(
+            allocator,
+            file_size,
+            null,
+            @alignOf(u8),
+            0,
+        );
 
         return file_contents;
     }
 
     // -------------------------------------------------------------------------------- //
 };
+
+// -------------------------------------------------------------------------------- //
+// API TIME
+// -------------------------------------------------------------------------------- //
+
+var context: Context = undefined;
+
+pub fn init(allocator: std.mem.Allocator, locale: []const u8) !Context {
+    context = try Context.init(allocator, locale);
+
+    return context;
+}
+
+pub fn translate(msgid: []const u8) []const u8 {
+    return msgid;
+}
+
+// -------------------------------------------------------------------------------- //
 
 test "translator" {
     var translator = try init(testing.allocator, "en_US.UTF-8");
